b4475673c5fb2de3cb56937f56e0670cbd50a4b8
Hossam Reda <rdhosam712@gmail.com>
2025-10-01 05:00:29 +0300

    update interval

--- src/pages/Images.tsx
+++ src/pages/Images.tsx
@@ -5,54 +5,64 @@ interface IDate {
 }
 const Images = () => {
     const [count, setCount] = useState<number>(0);
-    const [data, setDate] = useState<IDate[]>([]);
+    const [data, setData] = useState<IDate[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
 
     useEffect(() => {
         console.log("Effect running, starting interval...");
-        if (count > 5) {
+        const interval = setInterval(() => {
+            setCount((prev) => {
+                if (prev >= 5) {
+                    clearInterval(interval);
+                    return prev;
+                } else {
+                    return prev + 1
+                }
+            });
+        }, 1000);
 
-            const newDate = Array.from({ length: 50 }, (_, i) => ({
+        return () => {
+            console.log("Cleaning up, clearing interval!");
+            clearInterval(interval);
+        };
+    }, [count]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        if (count >= 5) {
+
+            const newData = Array.from({ length: 50 }, (_, i) => ({
                 id: i + 1,
                 thumbnail: `https://picsum.photos/300/${200 + (i % 10) * 30}`,
             }));
-            setDate(newDate);
+
+            setData(newData);
+            setIsLoading(false);
         } else {
-            setDate([]);
+            setData([]);
         }
-        // cleanup
-        return () => {
-            console.log("Cleaning up, clearing interval!");
-        };
-    }, [count]); // [] يعني يشتغل مرّة واحدة عند mount
+    }, [count]);
 
     return (
         <>
             <div className="space-x-2 mb-3">
                 <h1>counter : {count}</h1>
-                <button
-                    className="border rounded p-2"
-                    onClick={() => setCount(pr => pr + 1)}>
-                    Increment
-                </button>
-                <button
-                    className="border rounded p-2"
-                    onClick={() => setCount(pr => pr - 1)}>
-                    Decrement
-                </button>
             </div>
-            <div className="flex  justify-center  items-center min-h-screen overflow-x-hidden bg-[#181818]">
-                <div className="containers columns-3 gap-5 p-[50px_100px] [column-fill:_balance]">
-                    {data.map((item) => (
-                        <img
-                            key={item.id}
-                            src={item.thumbnail}
-                            alt={""}
-                            className="mt-2 w-full h-auto border-2 border-white/50 rounded-lg transition duration-300 ease-in-out hover:scale-105"
-                        />
-                    ))}
+            {isLoading ? <h1>Stop for 5 seconds</h1> :
+                <div className="flex  justify-center  items-center min-h-screen overflow-x-hidden bg-[#181818]">
+                    <div className="containers columns-3 gap-5 p-[50px_100px] [column-fill:_balance]">
+                        {data.map((item) => (
+                            <img
+                                key={item.id}
+                                src={item.thumbnail}
+                                alt={""}
+                                className="mt-2 w-full h-auto border-2 border-white/50 rounded-lg transition duration-300 ease-in-out hover:scale-105"
+                            />
+                        ))}
+                    </div>
                 </div>
-            </div>
+            }
         </>
     );
 };
